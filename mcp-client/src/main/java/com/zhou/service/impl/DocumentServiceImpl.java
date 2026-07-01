f89969f1f3f861cd88e57d2efefa63c8ea4bf0c0
zhou <314150624@qq.com>
2026-07-01 11:04:39 +0800

    dmc

--- mcp-client/src/main/java/com/zhou/service/impl/DocumentServiceImpl.java
+++ mcp-client/src/main/java/com/zhou/service/impl/DocumentServiceImpl.java
@@ -1,21 +1,35 @@
 package com.zhou.service.impl;
 
 import com.zhou.service.DocumentService;
+import com.zhou.utils.CustomTextSplitter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DocumentServiceImpl implements DocumentService {
+
+    private final RedisVectorStore vectorStore;
+
     @Override
     public List<Document> uploadDocument(Resource resource, String fileName) {
         TextReader textReader = new TextReader(resource);
         textReader.getCustomMetadata().put("fileName",fileName);
         List<Document> documents = textReader.get();
         System.out.println("documentList:" + documents);
+
+        CustomTextSplitter textSplitter = new CustomTextSplitter();
+        List<Document> apply = textSplitter.apply(documents);
+
+        vectorStore.add(apply);
+
+
         return documents;
     }
 
