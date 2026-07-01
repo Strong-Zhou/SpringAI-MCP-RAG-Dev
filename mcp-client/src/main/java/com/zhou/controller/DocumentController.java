1137de277ee9af9691169e9100d4b658c8b8b7ab
zhou <314150624@qq.com>
2026-07-01 09:28:38 +0800

    dmc

--- mcp-client/src/main/java/com/zhou/controller/DocumentController.java
+++ mcp-client/src/main/java/com/zhou/controller/DocumentController.java
@@ -4,6 +4,7 @@ package com.zhou.controller;
 import com.zhou.service.DocumentService;
 import com.zhou.utils.LeeResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,8 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
-    @RequestMapping("/uploadRagDoc")
-    public LeeResult uploadRagDoc(@RequestParam MultipartFile multipartFile) {
+    @PostMapping("/uploadRagDoc")
+    public LeeResult uploadRagDoc(@RequestParam("file") MultipartFile multipartFile) {
         documentService.uploadDocument(multipartFile.getResource(), multipartFile.getOriginalFilename());
         return LeeResult.ok();
     }
